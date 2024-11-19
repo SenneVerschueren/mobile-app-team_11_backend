49904fc33c339bbc265686cc3a1b097d16bc7164
jagoonzea <jago.onzea@hotmail.com>
2024-11-19 13:07:31 +0100

    FC

---
+++ src/main/java/mobileapplications/workoutbuilder/WorkoutbuilderApplication.java
@@ -0,0 +1,13 @@
+package mobileapplications.workoutbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutbuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutbuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mobileapplications/workoutbuilder/WorkoutbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package mobileapplications.workoutbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutbuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
