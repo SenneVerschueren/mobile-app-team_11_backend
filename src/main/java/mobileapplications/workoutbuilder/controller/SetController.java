614f38c66f9536c00c4ba9d4f40f4936e6892275
jagoonzea <jago.onzea@hotmail.com>
2024-12-04 09:31:50 +0100

    Added cross origin *

--- src/main/java/mobileapplications/workoutbuilder/controller/SetController.java
+++ src/main/java/mobileapplications/workoutbuilder/controller/SetController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/sets")
+@CrossOrigin(origins = "*")
 public class SetController {
 
     @Autowired
--- src/main/java/mobileapplications/workoutbuilder/controller/WorkoutController.java
+++ src/main/java/mobileapplications/workoutbuilder/controller/WorkoutController.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/workouts")
+@CrossOrigin(origins = "*")
 public class WorkoutController {
 
     @Autowired
