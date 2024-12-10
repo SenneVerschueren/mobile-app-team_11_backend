0584cd5641d00d067e4d591f5b0ea4edbdc8cb27
SenneVerschueren <senne.verschueren1@student.ucll.be>
2024-12-10 16:14:57 +0100

    expo url update

--- src/main/java/mobileapplications/workoutbuilder/security/SecurityConfiguration.java
+++ src/main/java/mobileapplications/workoutbuilder/security/SecurityConfiguration.java
@@ -31,11 +31,11 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8081", "exp://192.168.0.143:8081")); // Replace
-                                                                                                             // with
-                                                                                                             // your
-                                                                                                             // frontend
-                                                                                                             // URL
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8081", "http://192.168.0.143:8081")); // Replace
+                                                                                                              // with
+                                                                                                              // your
+                                                                                                              // frontend
+                                                                                                              // URL
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // Enable cookies/authentication if needed
