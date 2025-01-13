4ba7bc5c8bbe07a55ba8ced37529aae71ea384b2
SenneVerschueren <senne.verschueren1@student.ucll.be>
2025-01-13 18:05:22 +0100

    fix build issue

--- src/test/java/mobileapplications/workoutbuilder/WorkoutbuilderApplicationTests.java
+++ src/test/java/mobileapplications/workoutbuilder/WorkoutbuilderApplicationTests.java
@@ -2,8 +2,10 @@ package mobileapplications.workoutbuilder;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WorkoutbuilderApplicationTests {
 
 	@Test
